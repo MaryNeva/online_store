eafd449d8cfb27f83050416d1969d18e531fd55f
MaryNeva <mariianevodova@gmail.com>
2023-12-21 13:39:59 +0400

    first commit / create project

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'title': 'Store',
+        'is_promotion': True,
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    return render(request, 'products/products.html')
