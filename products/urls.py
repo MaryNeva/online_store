c2220510c4e937b155d948b925fd2fda3c016362
MaryNeva <mariianevodova@gmail.com>
2023-12-22 17:23:36 +0400

    fourth commit / add users app

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,12 @@
 
 from django.urls import path
-from products import views
-from django.conf.urls.static import static
-from django.conf import settings
+from products.views import products
 
+app_name = 'products'
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
+    path('', products, name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- store/urls.py
+++ store/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('', index, name='index'),
+    path('products/', include('products.urls', namespace="products")),
+    path('users/', include('users.urls', namespace="users")),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
