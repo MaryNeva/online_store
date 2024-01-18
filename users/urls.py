f52ff99af85265cc51c8ba900ba77e2aa23ee115
MaryNeva <mariianevodova@gmail.com>
2024-01-18 16:44:23 +0400

    ten commit / review code

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth.views import LogoutView
 
 app_name = 'users'
 
-
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
