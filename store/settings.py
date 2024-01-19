e1b34b285afa7115db4892fc9b37fa93161e566a
maryneva <mariianevodova@gmail.com>
2024-01-19 23:12:36 +0400

    12 commit / created model EmailVerification

--- store/settings.py
+++ store/settings.py
@@ -134,3 +134,6 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
